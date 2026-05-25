6a86a00968ec06079f7d26de2f00d4e26f31ab3c
HobbitQia <861394755@qq.com>
2026-05-25 20:40:49 +0800

    enable gemmmini

--- generators/chipyard/src/main/scala/config/RoCCAcceleratorConfigs.scala
+++ generators/chipyard/src/main/scala/config/RoCCAcceleratorConfigs.scala
@@ -12,6 +12,40 @@ class CGRARocketConfig extends Config(
   new freechips.rocketchip.rocket.WithNBigCores(1) ++
   new chipyard.config.AbstractConfig)
 
+object CGRAMinimalGemminiRocketConfig {
+  val minimalGemminiConfig:
+    gemmini.GemminiArrayConfig[chisel3.SInt, gemmini.Float, gemmini.Float] =
+    gemmini.GemminiConfigs.defaultConfig.copy(
+    dataflow = gemmini.Dataflow.WS,
+    has_training_convs = false,
+    has_max_pool = false,
+    has_nonlinear_activations = false,
+    has_dw_convs = false,
+    has_normalizations = false,
+    has_first_layer_optimizations = false,
+    has_loop_conv = false,
+    mvin_scale_args = None,
+    mvin_scale_acc_args = None,
+    mvin_scale_shared = false,
+    acc_scale_args = None,
+    acc_read_full_width = false,
+    acc_read_small_width = true,
+    ex_read_from_acc = false,
+    ex_write_to_spad = false,
+    hardcode_d_to_garbage_addr = true,
+    sp_capacity = gemmini.CapacityInKilobytes(64),
+    acc_capacity = gemmini.CapacityInKilobytes(32),
+    dma_maxbytes = 64,
+    dma_buswidth = 128)
+}
+
+class CGRAMinimalGemminiRocketConfig extends Config(
+  new chipyard.config.WithCGRA() ++
+  new gemmini.DefaultGemminiConfig(CGRAMinimalGemminiRocketConfig.minimalGemminiConfig) ++
+  new freechips.rocketchip.rocket.WithNBigCores(1) ++
+  new chipyard.config.WithSystemBusWidth(128) ++
+  new chipyard.config.AbstractConfig)
+
 class ReRoCCTestConfig extends Config(
   new rerocc.WithReRoCC ++
   new chipyard.config.WithCharacterCountRoCC ++                // rerocc tile4 is charcnt
